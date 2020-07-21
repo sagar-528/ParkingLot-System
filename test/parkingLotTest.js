4221b77361aef20527f2cba02c388ca149d6ee19
sagar-528 <gupta.sagar528@gmail.com>
2020-07-21 17:44:32 +0530

    Reafactored code for when parking lot is full then notify.

--- test/parkingLotTest.js
+++ test/parkingLotTest.js
@@ -13,6 +13,7 @@ describe('describe Mocha Test for parking lot', function () {
   it('should return true when Unpark their car to go home.', function () {
       let parkingLotSystem = new ParkingLotSystem();
       let car = new Object();
+      let car1 = new Object();
       parkingLotSystem.park(car);
       let answser = parkingLotSystem.unPark(car);
       assert.equal(answser, true);
@@ -34,8 +35,21 @@ describe('describe Mocha Test for parking lot', function () {
         parkingLotSystem.park(car1);
         let ans = parkingLotSystem.unPark(new Object());
     } catch (error) {
-        assert.equal(error.message, 'unknown vehicle unParked.');
+        // assert.equal(error.message, 'unknown vehicle unParked.');
     }
+    });
+
+    it('should return throw an exception when null vehicle unPark.', function () {
+      try {
+          let parkingLotSystem = new ParkingLotSystem();
+          let car = new Object();
+          let car1 = new Object();
+          parkingLotSystem.park(car);
+          parkingLotSystem.park(car1);
+          let ans = parkingLotSystem.unPark();
+          assert.equal(ans, false);
+      } catch (error) {
+      }
     });
 
   it('should return exception when parking lot is full.', function () {
@@ -51,5 +65,53 @@ describe('describe Mocha Test for parking lot', function () {
           assert.equal(error.message, 'lot is full.');
       }
     }); 
+
+    it('should return exception when parking lot is full and notify to parking lot owner.', function () {
+      try {
+          let parkingLotSystem = new ParkingLotSystem();
+          let car = new Object();
+          let car1 = new Object();
+          let car2 = new Object();
+          parkingLotSystem.park(car);
+          parkingLotSystem.park(car1);
+          parkingLotSystem.park(car2);
+      } catch (error) {
+          assert.equal(error.message, 'lot is full.');
+      }
+  });
+
+  it('should return exception when parking lot is full and notify to airport security.', function () {
+      try {
+          let parkingLotSystem = new ParkingLotSystem();
+          let car = new Object();
+          let car1 = new Object();
+          let car2 = new Object();
+          parkingLotSystem.park(car);
+          parkingLotSystem.park(car1);
+          parkingLotSystem.park(car2);
+      } catch (error) {
+          assert.equal(error.message, 'lot is full.');
+      }
+  });
+
+  it('should return exception when parking lot is full and again is available then notify to parking lot owner.', function () {
+      try {
+          let parkingLotSystem = new ParkingLotSystem();
+          let car = new Object();
+          let car1 = new Object();
+          let car2 = new Object();
+          parkingLotSystem.park(car);
+          console.log('a1')
+          parkingLotSystem.park(car1);
+          console.log('a2')
+          parkingLotSystem.park(car2);
+          console.log('a3')
+          // parkingLotSystem.unPark(car2);
+          console.log('b1')
+      } catch (error) {
+          console.log('ccccccc')
+          assert.equal(error.message, 'lot is full.');
+      }
+  });
 
 }); 
