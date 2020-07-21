6ed5ace4e49ef85c8f8b07da29028a687bad1d70
sagar-528 <gupta.sagar528@gmail.com>
2020-07-21 13:13:37 +0530

    Added: code for unpark the car to go home.

--- ParkingLotSystem.js
+++ ParkingLotSystem.js
@@ -1,7 +1,16 @@
 class ParkingLotSystem {
 
     park(vehicle){
+        this.parkvehicle = vehicle;
         return true;
     }
+
+    unPark(vehicle) {
+        if (this.parkVehicle == vehicle) {
+            return true;
+        }
+        return false;
+    }
+    
 }
 module.exports = ParkingLotSystem;
--- test/parkingLotTest.js
+++ test/parkingLotTest.js
@@ -6,8 +6,16 @@ describe('describe Mocha Test for parking lot', function () {
   it('should return park their car to catch the flight.', function () {
         let parkingLotSystem = new ParkingLotSystem();
         let car = new Object();
-        let ans = parkingLotSystem.park(car);
-        assert.equal(ans, true);
+        let answser = parkingLotSystem.park(car);
+        assert.equal(answser, true);
+    });
+
+    it('should return Unpark their car to go home.', function () {
+      let parkingLotSystem = new ParkingLotSystem();
+      let car = new Object();
+      parkingLotSystem.park(car);
+      let answser = parkingLotSystem.unPark(car);
+      assert.equal(answser, true);
     });
 
 }); 
