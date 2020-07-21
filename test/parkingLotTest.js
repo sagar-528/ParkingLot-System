1add2c60d2ebf282cc3da8e04fe8119225a9e38b
sagar-528 <gupta.sagar528@gmail.com>
2020-07-21 23:51:21 +0530

    ADDED: TestCase for know exact time when car is parking to parking lot owner.

--- test/parkingLotTest.js
+++ test/parkingLotTest.js
@@ -170,4 +170,13 @@ describe(`describe Mocha Test for parking lot`, () => {
       assert.equal(findSlots, false)
     });
 
+    //------------------uc8--------------------
+    it(`should return true when driver parked car then charges to be apply. `, () => {
+      let car = [new Object(0), new Date()];
+      let car1 = [new Object(1), new Date()];
+      parkingLotSystem.park(car)
+      let data = parkingLotSystem.park(car1)
+      assert.equal(data, true)
+    });
+
 }); 
